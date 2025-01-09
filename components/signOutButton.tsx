1fdab932aaea93b734f482feb1146d0b04a58e71
Olaf1303 <olaf.koziara@gmail.com>
2025-01-09 21:52:16 +0100

    Style

--- components/signOutButton.tsx
+++ components/signOutButton.tsx
@@ -1,6 +1,7 @@
 'use client'
 import {ReactNode} from "react";
 import {signOut} from "next-auth/react";
+import {Button} from "@/components/ui/button";
 
 export const SignOut = ({children}: { children: ReactNode }) => {
     return (
@@ -9,7 +10,7 @@ export const SignOut = ({children}: { children: ReactNode }) => {
                 await signOut({redirect: true})
             }}
         >
-            <button type="submit">{children}</button>
+            <Button type="submit">{children}</Button>
         </form>
     )
 }
