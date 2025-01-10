6bb9fffb9411461195789cdc6b20a6e15abd978e
Olaf1303 <olaf.koziara@gmail.com>
2025-01-10 10:14:36 +0100

    add flex container

--- app/(root)/user/page.tsx
+++ app/(root)/user/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import {auth} from "@/auth";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 import {Button} from "@/components/ui/button";
@@ -8,26 +7,27 @@ const Page = async () => {
     const session = await auth()
     if (!session) return null
     return (
-
-        <Card>
-            <CardHeader>
-                <CardTitle>User Information</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <div className="flex items-center space-x-4">
-                    <img src={session.user?.image ?? undefined} alt={`${session.user?.name}'s profile`}
-                         className="w-16 h-16 rounded-full"/>
-                    <div>
-                        <h2 className="text-lg font-bold">{session.user?.name}</h2>
-                        <p className="text-sm text-gray-600">{session.user?.email}</p>
+        <div className='flex justify-center'>
+            <Card>
+                <CardHeader>
+                    <CardTitle>User Information</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <div className="flex items-center space-x-4">
+                        <img src={session.user?.image ?? undefined} alt={`${session.user?.name}'s profile`}
+                             className="w-16 h-16 rounded-full"/>
+                        <div>
+                            <h2 className="text-lg font-bold">{session.user?.name}</h2>
+                            <p className="text-sm text-gray-600">{session.user?.email}</p>
+                        </div>
                     </div>
-                </div>
-                <Button className="mt-4">
-                    Change password
-                </Button>
+                    <Button className="mt-4">
+                        Change password
+                    </Button>
 
-            </CardContent>
-        </Card>
+                </CardContent>
+            </Card>
+        </div>
     );
 };
 
