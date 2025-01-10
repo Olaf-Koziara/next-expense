d2f262dbff86df88e0755c1b9526c633d44f0921
Olaf1303 <olaf.koziara@gmail.com>
2025-01-10 10:14:01 +0100

    expense category page

--- app/(root)/expenses/categories/page.tsx
+++ app/(root)/expenses/categories/page.tsx
@@ -1,32 +1,21 @@
 'use client'
 import React, {useEffect, useState} from 'react';
 import ExpenseIncomeCategoryForm from "@/components/form/ExpenseIncomeCategoryForm";
-import {ExpenseCategory} from "@/types/ExpenseCategory";
+import {Category} from "@/types/Category";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import {Trash} from "lucide-react";
+import useCategories from "@/hooks/useCategories";
 
 const Page = () => {
-    const [expenseCategories, setExpenseCategories] = useState<ExpenseCategory[]>([])
-    const fetchExpenseCategories = () => {
-        fetch('/api/expenseCategories').then(data => data.json().then(data => setExpenseCategories(data.expenseCategories)));
+    const {categories, deleteCategory, addCategory} = useCategories({type: 'expense'})
 
-    }
-    const deleteExpenseCategory = (_id: string) => {
-        fetch('/api/expenseCategories', {
-            method: 'DELETE',
-            body: JSON.stringify({_id})
-        }).then(() => fetchExpenseCategories());
-    }
-    useEffect(() => {
-        fetchExpenseCategories();
-    }, []);
     return (
         <div className='pt-3'>
             <h2 className='text-center text-3xl pb-3'>Categories</h2>
 
             <div className='w-1/2 mx-auto'>
                 <div className='pb-3'>
-                    <ExpenseIncomeCategoryForm type={'expense'} onCategoryAdded={fetchExpenseCategories}/>
+                    <ExpenseIncomeCategoryForm type={'expense'} onSubmit={addCategory}/>
                 </div>
                 <Table>
                     <TableHeader>
@@ -42,7 +31,7 @@ const Page = () => {
                     <TableBody>
 
                         {
-                            expenseCategories && expenseCategories.map(item =>
+                            categories.length > 0 && categories.map(item =>
                                 <TableRow key={item._id}>
                                     <TableCell>
                                         {item.name}
@@ -51,7 +40,7 @@ const Page = () => {
                                         {item.createdAt}
                                     </TableCell>
                                     <TableCell>
-                                        <button onClick={() => deleteExpenseCategory(item._id)}>
+                                        <button onClick={() => deleteCategory(item._id)}>
                                             <Trash/>
                                         </button>
                                     </TableCell>
@@ -64,7 +53,7 @@ const Page = () => {
                     </TableBody>
                 </Table>
                 {
-                    !expenseCategories.length && <h4 className='text-center pt-4'>No categories</h4>
+                    !categories.length && <h4 className='text-center pt-4'>No categories</h4>
                 }
             </div>
         </div>
