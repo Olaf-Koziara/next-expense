23253e48c42ff0dd3e0ce7d8b4b50c92f6ffc188
Olaf1303 <olaf.koziara@gmail.com>
2025-01-10 10:14:24 +0100

    income page

--- app/(root)/incomes/page.tsx
+++ app/(root)/incomes/page.tsx
@@ -3,13 +3,19 @@ import React, {useEffect} from 'react';
 import {useWallet} from "@/context/WalletContext";
 import {Income} from "@/types/Income";
 import ExpenseIncomeTable from "@/components/ExpenseIncomeTable";
+import ExpenseIncomeForm from "@/components/form/ExpenseIncomeForm";
 
 const Page = () => {
- 
+
 
     return (
-        <div>
-            <ExpenseIncomeTable type={'income'}/>
+        <div className="mt-5">
+            <div className="flex flex-col lg:flex-row gap-6">
+                <ExpenseIncomeTable type={'income'}/>
+                <div className="w-full lg:w-80">
+                    <ExpenseIncomeForm type='income'/>
+                </div>
+            </div>
         </div>
     );
 };
