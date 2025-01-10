4956626ff620502531b1535e158d24a1b5a0c08e
Olaf1303 <olaf.koziara@gmail.com>
2025-01-10 21:05:14 +0100

    wallet context update with services

--- context/WalletContext.tsx
+++ context/WalletContext.tsx
@@ -1,52 +1,80 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
-import { Wallet } from "@/types/Wallet";
-import { api } from "@/app/services/api";
-import { walletsSercice } from "@/app/services/wallets";
+import React, {createContext, useContext, useEffect, useState} from "react";
+import {useSession} from "next-auth/react";
+import {Wallet} from "@/types/Wallet";
+import {api} from "@/app/services/api";
+import {walletsService} from "@/app/services/wallets";
+import {Expense} from "@/types/Expense";
+import {expensesService} from "@/app/services/expenses";
+import {Income} from "@/types/Income";
+import {incomesService} from "@/app/services/incomes";
 
 type WalletContextType = {
-  wallets: Wallet[];
-  selectedWallet: Wallet | null;
-  setSelectedWallet: (wallet: Wallet) => void;
-  addWallet: (wallet: { name: string }) => Promise<void>;
+    wallets: Wallet[];
+    selectedWallet: Wallet | null;
+    setSelectedWallet: (wallet: Wallet) => void;
+    addWallet: (wallet: { name: string }) => Promise<void>;
+    addExpense: (expense: Expense) => Promise<void>;
+    addIncome: (expense: Expense) => Promise<void>;
+    deleteExpense: (_id: string) => Promise<void>;
+    deleteIncome: (_id: string) => Promise<void>;
 };
 
 const WalletContext = createContext<WalletContextType | undefined>(undefined);
+const withWalletCheck = <T, >(
+    selectedWallet: Wallet | null,
+    action: (walletId: string, data: T) => Promise<any>
+) => {
+    return (data: T) => {
+        if (!selectedWallet) return Promise.reject(new Error("No wallet selected"));
+        return action(selectedWallet._id, data)
+    }
+};
+export const WalletProvider = ({children}: { children: React.ReactNode }) => {
+    const {data: session} = useSession();
+    const [wallets, setWallets] = useState<Wallet[]>([]);
+    const [selectedWallet, setSelectedWallet] = useState<Wallet | null>(null);
 
-export const WalletProvider = ({ children }: { children: React.ReactNode }) => {
-  const { data: session } = useSession();
-  const [wallets, setWallets] = useState<Wallet[]>([]);
-  const [selectedWallet, setSelectedWallet] = useState<Wallet | null>(null);
+    useEffect(() => {
+        if (!session) return;
 
-  useEffect(() => {
-    if (!session) return;
+        walletsService.getAll().then((data) => {
+            setWallets(data);
+            setSelectedWallet(data[0] || null);
+        });
+    }, [session]);
 
-    walletsSercice.getAll().then((data) => {
-      setWallets(data);
-      setSelectedWallet(data[0] || null);
-    });
-  }, [session]);
+    const addWallet = async (wallet: { name: string }) => {
+        if (!session) return;
+        const newWallet = await walletsService.add(wallet);
+        setWallets((prevWallets) => [...prevWallets, newWallet]);
+    };
+    const addExpense = withWalletCheck(selectedWallet, expensesService.add)
+    const addIncome = withWalletCheck(selectedWallet, incomesService.add)
+    const deleteExpense = withWalletCheck(selectedWallet, expensesService.deleteOne)
+    const deleteIncome = withWalletCheck(selectedWallet, incomesService.deleteOne)
 
-  const addWallet = async (wallet: { name: string }) => {
-    if (!session) return;
-
-    const newWallet = await walletsSercice.add(wallet);
-    setWallets((prevWallets) => [...prevWallets, newWallet]);
-  };
-
-  return (
-    <WalletContext.Provider
-      value={{ wallets, selectedWallet, setSelectedWallet, addWallet }}
-    >
-      {children}
-    </WalletContext.Provider>
-  );
+    return (
+        <WalletContext.Provider
+            value={{
+                wallets,
+                selectedWallet,
+                setSelectedWallet,
+                addWallet,
+                addExpense,
+                deleteExpense,
+                addIncome,
+                deleteIncome
+            }}
+        >
+            {children}
+        </WalletContext.Provider>
+    );
 };
 
 export const useWallet = () => {
-  const context = useContext(WalletContext);
-  if (context === undefined) {
-    throw new Error("useWallet must be used within a WalletProvider");
-  }
-  return context;
+    const context = useContext(WalletContext);
+    if (context === undefined) {
+        throw new Error("useWallet must be used within a WalletProvider");
+    }
+    return context;
 };
