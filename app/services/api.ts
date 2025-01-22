cd9fdb53e2e55e60d261f0ad33e6684c389f6eef
Olaf1303 <olaf.koziara@gmail.com>
2025-01-22 17:16:12 +0100

    add type queryparams

--- app/services/api.ts
+++ app/services/api.ts
@@ -2,7 +2,7 @@ import {SearchParams} from "next/dist/server/request/search-params";
 
 export const BASE_URL = process.env.NEXT_PUBLIC_HOSTNAME;
 
-async function GET<T>(_url: string, params?: Record<string, string>): Promise<T> {
+async function GET<T>(_url: string, params?: QueryParams): Promise<T> {
     const queryString = new URLSearchParams(params).toString();
     const url = queryString ? `${_url}/?${queryString}` : _url;
     const res = await fetch(url, {
@@ -50,6 +50,7 @@ async function DELETE<T, K>(url: string, data: T): Promise<K> {
     return res.json();
 }
 
+export type QueryParams = Record<string, string>;
 export const api = {
     GET,
     POST,
