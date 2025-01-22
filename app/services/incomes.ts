40793f2e42a544f070a4a3d5e8935d1edcb79463
Olaf1303 <olaf.koziara@gmail.com>
2025-01-22 18:09:28 +0100

    Update incomes.ts

--- app/services/incomes.ts
+++ app/services/incomes.ts
@@ -9,10 +9,10 @@ type AddIncomeData = {
     selectedWalletId: string,
 } & Income;
 const add = async (walletId: string, income: Income) => {
-    return api.POST<AddIncomeData, void>(endpoints.income, {selectedWalletId: walletId, ...income})
+    return api.POST<AddIncomeData>(endpoints.income, {selectedWalletId: walletId, ...income})
 }
 const deleteOne = async (walletId: string, expenseId: string) => {
-    return api.DELETE<{ walletId: string, _id: string }, void>(endpoints.income, {walletId: walletId, _id: expenseId})
+    return api.DELETE<{ walletId: string, _id: string }>(endpoints.income, {walletId: walletId, _id: expenseId})
 }
 export const incomesService = {
     getAll, add, deleteOne
