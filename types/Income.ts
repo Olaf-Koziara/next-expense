3fb0305ecb5eb6b835316e0cc8c4c33fa4c6e0d0
Olaf1303 <olaf.koziara@gmail.com>
2025-01-29 10:32:56 +0100

    new type

--- types/Income.ts
+++ types/Income.ts
@@ -3,3 +3,4 @@ import {Expense} from "@/types/Expense";
 export type Income = Expense;
 export type Transaction = Income | Expense;
 export type TransactionType = 'income' | 'expense';
+export type IncomeWithWalletId = Income & { walletId: string };
