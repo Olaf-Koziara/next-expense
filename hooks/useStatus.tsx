679b99ddc0ac467555216efc75d63cce939f37f7
Olaf1303 <olaf.koziara@gmail.com>
2025-01-29 10:38:18 +0100

    use status hook

---
+++ hooks/useStatus.tsx
@@ -0,0 +1,22 @@
+import {useState, useCallback} from "react";
+
+const useStatus = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
+
+    const startLoading = useCallback(() => setIsLoading(true), []);
+    const stopLoading = useCallback(() => setIsLoading(false), []);
+    const resetError = useCallback(() => setError(null), []);
+    const setErrorState = useCallback((err: Error) => setError(err), []);
+
+    return {
+        isLoading,
+        error,
+        startLoading,
+        stopLoading,
+        resetError,
+        setErrorState,
+    };
+};
+
+export default useStatus;
