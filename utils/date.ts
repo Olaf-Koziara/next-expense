c04b144901718d77ae9c35d0c65ee24b7f0c2eaa
Olaf1303 <olaf.koziara@gmail.com>
2025-02-08 09:19:36 +0100

    add dates variables

--- utils/date.ts
+++ utils/date.ts
@@ -1,6 +1,18 @@
 import {DateRange} from "react-day-picker";
 import {format} from "date-fns";
 
+export const now = new Date();
+export const oneYearAgo = new Date();
+oneYearAgo.setFullYear(now.getFullYear() - 1);
+export const sixMonthsAgo = new Date();
+sixMonthsAgo.setMonth(now.getMonth() - 6);
+export const threeMonthsAgo = new Date();
+threeMonthsAgo.setMonth(now.getMonth() - 3);
+export const oneMonthAgo = new Date();
+oneMonthAgo.setMonth(now.getMonth() - 1);
+export const oneWeekAgo = new Date();
+oneWeekAgo.setDate(now.getDate() - 7);
+
 export const dateRangeValuesToString = (dateRange: DateRange) => {
     if (!dateRange.from) return {dateFrom: '', dateTo: ''}
     return {
@@ -8,3 +20,6 @@ export const dateRangeValuesToString = (dateRange: DateRange) => {
         dateTo: dateRange.to ? format(dateRange.to, 'yyyy-MM-dd') : ''
     }
 }
+export const getRandomDate = (start: Date, end: Date) => {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+};
