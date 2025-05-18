74c856cb66b47fddb880ea62c0d647199e2ae58d
Olaf1303 <olaf.koziara@gmail.com>
2025-05-18 18:31:51 +0200

    Date range type and utils

--- utils/date.ts
+++ utils/date.ts
@@ -13,13 +13,17 @@ oneMonthAgo.setMonth(now.getMonth() - 1);
 export const oneWeekAgo = new Date();
 oneWeekAgo.setDate(now.getDate() - 7);
 
-export const dateRangeValuesToString = (dateRange: DateRange) => {
-    if (!dateRange.from) return {dateFrom: '', dateTo: ''}
-    return {
-        dateFrom: format(dateRange.from, 'yyyy-MM-dd'),
-        dateTo: dateRange.to ? format(dateRange.to, 'yyyy-MM-dd') : ''
+export const dateRangeValuesToString = (dateRange: { from: Date; to: Date }): string => {
+    const params = new URLSearchParams();
+    if (dateRange.from) {
+        params.append('dateFrom', dateRange.from.toISOString());
     }
-}
+    if (dateRange.to) {
+        params.append('dateTo', dateRange.to.toISOString());
+    }
+    return params.toString();
+};
+
 export const getRandomDate = (start: Date, end: Date) => {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 };
