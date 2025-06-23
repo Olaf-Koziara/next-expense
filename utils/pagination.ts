96d1d65c4c95e1fa56f3b4d6d0337f022ff32178
Olaf1303 <olaf.koziara@gmail.com>
2025-06-23 10:25:11 +0200

    pagination utils

--- utils/pagination.ts
+++ utils/pagination.ts
@@ -1,5 +1,6 @@
 export const getPaginationFromUrl = (url: URL): { skip: number, pageSize: number } => {
-    const skip = parseInt(url.searchParams.get('pageIndex') ?? '0');
+    const pageIndex = parseInt(url.searchParams.get('pageIndex') ?? '0');
     const pageSize = parseInt(url.searchParams.get('pageSize') ?? '10');
+    const skip = pageIndex * pageSize;
     return {skip, pageSize};
 }
