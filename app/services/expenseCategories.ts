595207efe85263bc38e23f32f4b265c83601106d
Olaf1303 <olaf.koziara@gmail.com>
2025-06-23 10:41:10 +0200

    services

--- app/services/expenseCategories.ts
+++ app/services/expenseCategories.ts
@@ -1,21 +1,7 @@
-import {api} from "@/app/services/api";
-import {Category} from "@/types/Category";
-import {endpoints} from "@/app/services/endpoints";
-import {ResponseWithArray, Service} from "@/types/Service";
+import { Category } from "@/types/Category";
+import { endpoints } from "@/app/services/endpoints";
+import { Service } from "@/types/Service";
+import { createCategoryService } from "./categoryService";
 
-const getAll = async () => {
-    return api.GET<ResponseWithArray<Category>>(endpoints.expenseCategory)
-}
-const add = async (category: Category) => {
-    return api.POST<Category>(endpoints.expenseCategory, category)
-}
-const remove = async (_id: string) => {
-    return api.DELETE<{ _id: string }>(endpoints.expenseCategory, {_id})
-}
-const patch = async (data: Partial<Category>) => {
-    return api.PATCH<Category>(endpoints.expenseCategory, {...data});
-}
-
-export const expenseCategoriesService: Service<Category> = {
-    getAll, add, remove, patch
-}
+export const expenseCategoriesService: Service<Category> =
+  createCategoryService(endpoints.expenseCategory);
